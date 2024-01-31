a6370c647d2f6d3ffb648087d1a29b4e1b51c145
hongbowu <wuhongbo87@gmail.com>
2024-01-31 16:48:49 -0500

    add logic for put method

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -66,8 +66,8 @@ module.exports = () => {
               presets: ['@babel/preset-env'],
               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
-          }
-        }
+          },
+        },
       ],
     },
   };
