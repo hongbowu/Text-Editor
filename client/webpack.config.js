cc59f733c5c96e4a31a1becdd867398c982f6ea6
hongbowu <wuhongbo87@gmail.com>
2024-01-31 16:21:02 -0500

    Add CSS loaders and babel to webpack.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,7 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = () => {
   return {
     mode: 'development',
